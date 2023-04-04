1b251bb1b5fc05757f1a9947b7fb4c753bab425e
Hamza <hamzasiddiqui3792gmail.com>
2023-04-04 05:21:46 +0500

    first commit

--- lib/ui/posts/add_post.dart
+++ lib/ui/posts/add_post.dart
@@ -2,6 +2,8 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebasebasics/widgets/rounded_button.dart';
 import 'package:flutter/material.dart';
 
+import '../../utils/utility.dart';
+
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({Key? key}) : super(key: key);
 
@@ -11,6 +13,7 @@ class AddPostScreen extends StatefulWidget {
 
 class _AddPostScreenState extends State<AddPostScreen> {
 
+  final postController = TextEditingController();
   bool loading = false;
   final databaseRef = FirebaseDatabase.instance.ref('Post');
   @override
@@ -26,11 +29,34 @@ class _AddPostScreenState extends State<AddPostScreen> {
             const SizedBox(height: 30.0,),
 
             TextFormField(
+              controller: postController,
               maxLines: 3,
               decoration: const InputDecoration(hintText: 'Whats in your mind?'),
             ),
             const SizedBox(height: 30,),
-            RoundedButton(title: 'Add post', onTap: () {}),
+            RoundedButton(
+                title: 'Add post',
+                loading: loading,
+                onTap: () {
+                  setState(() {
+                    loading = true;
+                  });
+              databaseRef.child(DateTime.now().millisecondsSinceEpoch.toString()).set({
+                'id': DateTime.now().millisecondsSinceEpoch.toString(),
+                'title' : postController.text.toString(),
+              }).then((value) {
+
+                Utils().ToastMessage('Post added successfully');
+                setState(() {
+                  loading = false;
+                });
+              }).onError((error, stackTrace) {
+                Utils().ToastMessage(error.toString());
+                setState(() {
+                  loading = false;
+                });
+              });
+            }),
 
           ],
         ),
